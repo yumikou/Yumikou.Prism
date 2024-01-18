1ff7d03ee00b979560e7d6ebcdcd2f0dce18fed8
yumikou <dupsya@163.com>
2024-01-19 01:09:08 +0800

    add InteractionRequest

---
+++ samples/InteractionDialog/InteractionDialog.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace InteractionDialog.Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
